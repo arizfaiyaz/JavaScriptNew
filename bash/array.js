9243eaba8d9873410a1cf5e7ec36e38e6cdfde68
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-06-30 15:36:23 +0530

    Add initial content for a.txt, abc.txt, and command.txt; expand array.js with fruit and numbers examples

---
+++ bash/array.js
@@ -0,0 +1,9 @@
+let fruit = ['apple', 'banana', 'cherry'];
+
+
+/// spread = ...
+
+let moreFruits = ['orange', ...fruit, 'grape'];
+console.log(moreFruits); // ['orange', 'apple', 'banana', 'cherry', 'grape']
+
+let numbers = [1, 2, 3,4,5];
